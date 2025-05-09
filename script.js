a59b92b0760bcbfcd41628aa564c3d360d8f1d93
SUDHANSHU RANJAN <sudhanshuranjan0109@gmail.com>
2025-05-09 13:55:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const images = [
     'images/photo1.jpg',
     'images/photo2.jpg',
     'images/photo3.jpg',
-    'images/photo4.jpg'
+    'images/photo4.jpg',
+    'images/photo5.jpg',
+    'images/photo6.jpg',
+    'images/photo7.jpg',
+    'images/photo8.jpg',
+    'images/photo9.jpg',
+    'images/photo10.jpg',
+    'images/photo11.jpg',
+    'images/photo12.jpg',
   ];
   
   let currentIndex = 0;
